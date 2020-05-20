bfcdcd76758fb39c07905fe5a23878c9bb90c02b
Jorge Pizaña Mtz <a311049@uach.mx>
2020-05-20 01:21:01 -0600

    Texto de Login

--- lib/screens/login/index.dart
+++ lib/screens/login/index.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -13,11 +14,29 @@ class _LoginScreenState extends State<LoginScreen> {
           //title: Text('login screen'),
       //),
       body: Container(
+        width: double.infinity,
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage('lib/assets/background_images/login_screen_back.png'),
             fit: BoxFit.cover
           ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 80,),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Login', style: TextStyle(color: Colors.white, fontSize: 40),),
+                  Text('Welcome', style: TextStyle(color: Colors.white, fontSize: 20),),
+                ],
+              ),
+            )
+
+          ],
         ),
       ),
     );
