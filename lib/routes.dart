5a7afc56a0f376d21441c731e7fc053d51d1e996
Jorge Pizaña Mtz <a311049@uach.mx>
2020-05-19 23:52:04 -0600

    Creación de ruta login
    Creación de rutas login y archivo login.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
 import './screens/home/index.dart';
+import './screens/login/index.dart';
 
 const String homeRoute = 'home';
+const String loginRoute = 'login';
 
 class Router {
   static Route <dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case homeRoute:
         return MaterialPageRoute(builder: (_) => HomeScreen(title: 'Setlist Hero',));
-        
+
+      case loginRoute:
+        return MaterialPageRoute(builder: (_) => LoginScreen(title: 'Setlist Hero',));
+
       default:
         return MaterialPageRoute(
           builder: (context) => Scaffold(
