d23b2687389780e05028a94f92fc42e7d617f2a2
Jorge Pizaña Mtz <a311049@uach.mx>
2020-05-23 19:41:55 -0600

    Prototipo de menu principal
    Titulo con tabs de eventos actuales, este mes y el año

--- lib/screens/home/index.dart
+++ lib/screens/home/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:setlistherofl/screens/login/index.dart';
 import 'package:setlistherofl/screens/register/index.dart';
 
@@ -7,87 +8,91 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin{
+  TabController _tabController;
+
+  @override
+  void initState(){
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/Twenty One Pilots2.png'),
-                fit: BoxFit.cover,
-              ),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        centerTitle: true,
+        title: Text('Setlist Hero', 
+          style: TextStyle(
+            color: Colors.black87,
+            fontFamily: 'Montserrat',
             ),
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [
-                  Colors.black.withOpacity(.3),
-                  Colors.black.withOpacity(.3),
-                ]),
-              ),
-              child: Padding(
-                padding: EdgeInsets.all(30),
-                child: Center(
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Container(
-                          height: 40,
-                          child: RaisedButton(
-                            color: Colors.orangeAccent,
-                            child: Text(
-                              'LOGIN',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontFamily: 'Montserrat',
-                                  fontSize: 18),
-                            ),
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => LoginScreen()),
-                              );
-                            },
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: 40,),
-                      Expanded(
-                        child: Container(
-                          height: 40,
-                          child: RaisedButton(
-                            color: Colors.deepOrange,
-                            child: Text(
-                              'SIGNUP',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontFamily: 'Montserrat',
-                                  fontSize: 18),
-                            ),
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => RegisterScreen()),
-                              );
-                            },
-                          ),
-                        ),
-                      ),
-
-                    ],
-                  ),
-                ),
-              ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              FontAwesomeIcons.ellipsisV,
+              color: Colors.black87,
             ),
+            onPressed: (){},
           ),
         ],
       ),
+      body: ListView(
+        padding: EdgeInsets.only(left: 20.0),
+        children: <Widget>[
+          SizedBox(height: 15.0,),
+          Text(
+            'New events',
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'Montserrat',
+              fontSize: 30,
+            ),
+          ),
+          SizedBox(height: 15,),
+          TabBar(
+            controller: _tabController,
+            indicatorColor: Colors.transparent,
+            labelColor: Colors.orange[400],
+            isScrollable: true,
+            labelPadding: EdgeInsets.only(right: 45.0),
+            unselectedLabelColor: Colors.grey[400],
+            tabs: [
+              Tab(
+                child: Text(
+                    'Current Events',
+                  style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 21,
+                  ),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  'This Month',
+                  style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 21,
+                  ),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  'This Year',
+                  style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontSize: 21,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+
     );
   }
 }
