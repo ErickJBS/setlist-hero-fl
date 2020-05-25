15d6ffd7d73e57c5469fd48bdf239429c2cd4e91
Jorge Pizaña Mtz <a311049@uach.mx>
2020-05-24 20:22:49 -0600

    Error de bottom overflow arreglado
    Se agrego un widget SingleChildScrollView que contiene parte de los botones visuales, para evitarque aparezca el error de bottom overflow cada  vez que se utiliza  el teclado

--- lib/screens/login/index.dart
+++ lib/screens/login/index.dart
@@ -72,186 +72,181 @@ class _LoginScreenState extends State<LoginScreen> {
                 child: Padding(
                   key: _containerKey,
                   padding: EdgeInsets.all(30),
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(height: 15,),
-                      Padding(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 15,),
+                        Padding(
+                            padding: const EdgeInsets.only(bottom: 16.0),
+                            child: TextFormField(
+                                onChanged: (val){
+                                    setState(() {
+                                      email = val;
+                                    });
+                                },
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    labelText: 'Email',
+                                    //errorText: null,
+                                    labelStyle: TextStyle(
+                                      fontFamily: 'Montserrat',
+                                    ),
+                                ),
+                            ),
+                        ),
+                        Padding(
                           padding: const EdgeInsets.only(bottom: 16.0),
                           child: TextFormField(
-                            //validator: (val) => val.isEmpty ? 'Enter email' : null,
-                              //controller: controller,
-                              onChanged: (val){
-                                  setState(() {
-                                    email = val;
-                                  });
-                              },
-                              decoration: InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  labelText: 'Email',
-                                  //errorText: null,
-                                  labelStyle: TextStyle(
-                                    fontFamily: 'Montserrat',
-                                  ),
-                              ),
-                          ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 16.0),
-                        child: TextFormField(
-                          //validator: (val) => val.isEmpty ? 'Enter password' : null,
-                          //controller: controller,
-                          obscureText: true,
-                          onChanged: (val){
-                            setState(() {
-                              password = val;
-                            });
-                          },
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Password',
-                            //errorText: null,
-                            labelStyle: TextStyle(
-                              fontFamily: 'Montserrat',
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 20,),  // separador para botn login
-                      Container(
-                        //height: 40,
-                        //padding: EdgeInsets.all(10),
-                        //margin: EdgeInsets.symmetric(horizontal: 10),
-                        width: MediaQuery.of(context).size.width,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(topLeft: Radius.circular(5), topRight: Radius.circular(5), bottomLeft: Radius.circular(5), bottomRight: Radius.circular(5)),
-                          color: Colors.orangeAccent[700]
-                        ),
-                        child: FlatButton(
-                          onPressed: () async{
-                            dynamic result = await _auth.loginWithPassword(email, password);
-                            if (result == null){
+                            obscureText: true,
+                            onChanged: (val){
                               setState(() {
-                                error = 'Wrong email or password';
+                                password = val;
                               });
-                            }else{
-                              Navigator.popAndPushNamed(
-                                context, homeRoute);
-                            }
-                          },
-                          child: Text(
-                            'LOGIN',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: 'Montserrat',
-                                fontSize: 20),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 10,),
-                      Text(error,
-                        style: TextStyle(
-                          color: Colors.red,
-                          fontFamily: 'Montserrat',
-                        ),
-                      ),
-                      SizedBox(height: 20,),
-                      Text('Continue with social media',
-                        style: TextStyle(
-                          color: Colors.grey[700],
-                          fontFamily: 'Montserrat',
-                        ),
-                      ),
-                      SizedBox(height: 20,),
-                      Row(
-                        children: <Widget>[
-                          Expanded(
-                            child: Container(
-                              //height: 40,
-                              width: MediaQuery.of(context).size.width,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.only(topLeft: Radius.circular(5), topRight: Radius.circular(5), bottomLeft: Radius.circular(5), bottomRight: Radius.circular(5)),
-                                color: Color.fromARGB(255, 59, 89, 152)
-                              ),
-                              child: Center(
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    FlatButton(
-                                      onPressed: () => _loginWithSocialMedia(Provider.FACEBOOK),
-                                      child: Icon(
-                                        FontAwesomeIcons.facebook,
-                                        color: Colors.white,
-                                        size: 28,
-
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(width: 40,),  // separador para botones redes sociales
-                          Expanded(
-                            child: Container(
-                              //height: 40,
-                              width: MediaQuery.of(context).size.width,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.only(topLeft: Radius.circular(5), topRight: Radius.circular(5), bottomLeft: Radius.circular(5), bottomRight: Radius.circular(5)),
-                                  color: Colors.deepOrange,
-                              ),
-                              child: Center(
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    FlatButton(
-                                      onPressed: () => _loginWithSocialMedia(Provider.GOOGLE),
-                                      child: Icon(
-                                        FontAwesomeIcons.google,
-                                        color: Colors.white,
-                                        size: 28,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 60,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Text(
-                            "Don't have an account? ",
-                            style: TextStyle(
-                                color: Colors.grey[700],
-                                fontFamily: 'Montserrat',
-                            ),
-                          ),
-                          SizedBox(height: 5,),
-                          InkWell(
-                            onTap: (){
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => RegisterScreen()),
-                              );
                             },
-                            child: Text('Register',
-                              style: TextStyle(
-                                color: Colors.orangeAccent[400],
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Password',
+                              //errorText: null,
+                              labelStyle: TextStyle(
                                 fontFamily: 'Montserrat',
-                                fontWeight: FontWeight.bold,
-                                decoration: TextDecoration.underline,
                               ),
                             ),
-
                           ),
-                        ],
-                      ),
-                    ],
+                        ),
+                        SizedBox(height: 20,),  // separador para botn login
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(5), topRight: Radius.circular(5), bottomLeft: Radius.circular(5), bottomRight: Radius.circular(5)),
+                            color: Colors.orangeAccent[700]
+                          ),
+                          child: FlatButton(
+                            onPressed: () async{
+                              dynamic result = await _auth.loginWithPassword(email, password);
+                              if (result == null){
+                                setState(() {
+                                  error = 'Wrong email or password';
+                                });
+                              }else{
+                                Navigator.popAndPushNamed(
+                                  context, homeRoute);
+                              }
+                            },
+                            child: Text(
+                              'LOGIN',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 20),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 10,),
+                        Text(error,
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontFamily: 'Montserrat',
+                          ),
+                        ),
+                        SizedBox(height: 20,),
+                        Text('Continue with social media',
+                          style: TextStyle(
+                            color: Colors.grey[700],
+                            fontFamily: 'Montserrat',
+                          ),
+                        ),
+                        SizedBox(height: 20,),
+                        Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Container(
+                                //height: 40,
+                                width: MediaQuery.of(context).size.width,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.only(topLeft: Radius.circular(5), topRight: Radius.circular(5), bottomLeft: Radius.circular(5), bottomRight: Radius.circular(5)),
+                                  color: Color.fromARGB(255, 59, 89, 152)
+                                ),
+                                child: Center(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      FlatButton(
+                                        onPressed: () => _loginWithSocialMedia(Provider.FACEBOOK),
+                                        child: Icon(
+                                          FontAwesomeIcons.facebook,
+                                          color: Colors.white,
+                                          size: 28,
+
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 40,),  // separador para botones redes sociales
+                            Expanded(
+                              child: Container(
+                                //height: 40,
+                                width: MediaQuery.of(context).size.width,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.only(topLeft: Radius.circular(5), topRight: Radius.circular(5), bottomLeft: Radius.circular(5), bottomRight: Radius.circular(5)),
+                                    color: Colors.deepOrange,
+                                ),
+                                child: Center(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      FlatButton(
+                                        onPressed: () => _loginWithSocialMedia(Provider.GOOGLE),
+                                        child: Icon(
+                                          FontAwesomeIcons.google,
+                                          color: Colors.white,
+                                          size: 28,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 60,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              "Don't have an account? ",
+                              style: TextStyle(
+                                  color: Colors.grey[700],
+                                  fontFamily: 'Montserrat',
+                              ),
+                            ),
+                            SizedBox(height: 5,),
+                            InkWell(
+                              onTap: (){
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => RegisterScreen()),
+                                );
+                              },
+                              child: Text('Register',
+                                style: TextStyle(
+                                  color: Colors.orangeAccent[400],
+                                  fontFamily: 'Montserrat',
+                                  fontWeight: FontWeight.bold,
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
