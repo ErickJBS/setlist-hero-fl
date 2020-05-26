d93d21e97b998f4fadd40f9bce57f6a3cc0cca60
OscarOK <oscareom1a@gmail.com>
2020-05-26 12:30:19 -0600

    Song viewer screen init folders and files

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:setlistherofl/screens/song_viewer/index.dart';
 import 'package:setlistherofl/screens/splash/index.dart';
 
 import './screens/home/index.dart';
@@ -9,6 +10,7 @@ const String splashRoute = '/';
 const String homeRoute = 'Initial';
 const String registerRoute = 'register';
 const String loginRoute = 'login';
+const String songViewer = 'song_viewer';
 
 class Router {
   static Route <dynamic> generateRoute(RouteSettings settings) {
@@ -24,6 +26,9 @@ class Router {
         
       case loginRoute:
         return MaterialPageRoute(builder: (_) => LoginScreen());
+
+      case songViewer:
+        return MaterialPageRoute(builder: (_) => SongViewerScreen());
 
       default:
         return MaterialPageRoute(
---
+++ lib/screens/song_viewer/index.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SongViewerScreen extends StatefulWidget {
+  @override
+  _SongViewerState createState() => _SongViewerState();
+}
+
+class _SongViewerState extends State<SongViewerScreen> {
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("Hello"),
+      ),
+    );
+  }
+}
