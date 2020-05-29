848a2a13af99b5a7778d824009ace459e4e74230
Jorge Pizaña Mtz <a311049@uach.mx>
2020-05-29 00:49:00 -0600

    Update for better performance on the dashboard

--- lib/screens/home/index.dart
+++ lib/screens/home/index.dart
@@ -75,68 +75,71 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
           ),
         ],
       ),
-      body: ListView(
+      body: SingleChildScrollView(
         padding: EdgeInsets.only(left: 20.0),
-        children: <Widget>[
-          SizedBox(height: 10.0,),
-          Text(
-            'Events',
-            style: TextStyle(
-              color: Colors.black,
-              fontFamily: 'Montserrat',
-              fontSize: 30,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 10.0,),
+            Text(
+              'Events',
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Montserrat',
+                fontSize: 30,
+              ),
             ),
-          ),
-          SizedBox(height: 10,),
-          TabBar(
-              controller: _tabController,
-              indicatorColor: Colors.transparent,
-              labelColor: Colors.orange[400],
-              isScrollable: true,
-              labelPadding: EdgeInsets.only(right: 45.0),
-              unselectedLabelColor: Colors.grey[400],
-              tabs: [
-                Tab(
-                  child: Text(
-                    'Current Events',
-                    style: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 21,
+            SizedBox(height: 10,),
+            TabBar(
+                controller: _tabController,
+                indicatorColor: Colors.transparent,
+                labelColor: Colors.orange[400],
+                isScrollable: true,
+                labelPadding: EdgeInsets.only(right: 45.0),
+                unselectedLabelColor: Colors.grey[400],
+                tabs: [
+                  Tab(
+                    child: Text(
+                      'Current Events',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 21,
+                      ),
                     ),
                   ),
-                ),
-                Tab(
-                  child: Text(
-                    'This Month',
-                    style: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 21,
+                  Tab(
+                    child: Text(
+                      'This Month',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 21,
+                      ),
                     ),
                   ),
-                ),
-                Tab(
-                  child: Text(
-                    'This Year',
-                    style: TextStyle(
-                      fontFamily: 'Montserrat',
-                      fontSize: 21,
+                  Tab(
+                    child: Text(
+                      'This Year',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 21,
+                      ),
                     ),
-                  ),
+                  )
+                ]),
+            Container(
+                height: MediaQuery.of(context).size.height - 50,
+                width: double.infinity,
+                child: TabBarView(
+                    controller: _tabController,
+                    children: [
+                      EventPage(),
+                      EventPageMonth(),
+                      EventPageYear(),
+                    ]
                 )
-              ]),
-          Container(
-              height: MediaQuery.of(context).size.height - 50,
-              width: double.infinity,
-              child: TabBarView(
-                  controller: _tabController,
-                  children: [
-                    EventPage(),
-                    EventPageMonth(),
-                    EventPageYear(),
-                  ]
-              )
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
